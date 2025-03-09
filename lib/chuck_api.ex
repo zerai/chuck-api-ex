b58c5a3bd595bfac634bd5d98fdee0e22b707c04
zero <teclaizerai@gmail.com>
2025-03-09 15:18:50 +0100

    Init fn get_random_joke/0.

--- lib/chuck_api.ex
+++ lib/chuck_api.ex
@@ -1,6 +1,6 @@
 defmodule ChuckApi do
   @moduledoc """
-  Documentation for `ChuckApi`.
+  [Chuck Norris Joke](https://api.chucknorris.io/) API Client
   """
 
   @doc """
@@ -14,5 +14,18 @@ defmodule ChuckApi do
   """
   def hello do
     :world
+  end
+
+  @doc """
+  Get random joke.
+
+  """
+  def get_random_joke() do
+    url = "https://api.chucknorris.io/jokes/random"
+
+    response =
+      HTTPoison.get!(url)
+
+    Poison.decode!(response.body)
   end
 end
